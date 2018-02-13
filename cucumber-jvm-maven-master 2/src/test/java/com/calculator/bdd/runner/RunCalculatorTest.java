f0d91bd4897b08537d02d92350d80c361c292abe
codewarior <nayyer.shahbaz@aspose.com>
2018-02-13 05:53:52 +0500

    Add files via upload
    RunCalculatorTest.java is updated where un-necessary code for report generation is removed. The @CucumberOptions have been simplified to generate HTML report in.    target/cucumber-html-report.  folder

--- cucumber-jvm-maven-master 2/src/test/java/com/calculator/bdd/runner/RunCalculatorTest.java
+++ cucumber-jvm-maven-master 2/src/test/java/com/calculator/bdd/runner/RunCalculatorTest.java
@@ -3,15 +3,9 @@ package com.calculator.bdd.runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
-import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 
 @RunWith(Cucumber.class)
-@ExtendedCucumberOptions(jsonReport = "target/cucumber.json",
-overviewReport = true,
-outputFolder = "target")
 @CucumberOptions(
-        format = { "pretty", "html:target/Destination" },
         glue = "com.calculator.bdd.steps",
         features = "classpath:cucumber/calculator.feature",
         		plugin = { "html:target/cucumber-html-report",
