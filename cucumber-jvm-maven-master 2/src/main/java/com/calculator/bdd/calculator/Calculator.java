2a3b237c868df59fb34100fbeac495fe92bf8fad
codewarior <nayyer.shahbaz@aspose.com>
2018-02-16 21:58:16 +0500

    Add files via upload
    Methods are simplified for better understanding and un-necessary code lines are removed.

--- cucumber-jvm-maven-master 2/src/main/java/com/calculator/bdd/calculator/Calculator.java
+++ cucumber-jvm-maven-master 2/src/main/java/com/calculator/bdd/calculator/Calculator.java
@@ -1,15 +1,11 @@
 package com.calculator.bdd.calculator;
 
-
 import io.appium.java_client.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.*;
 import org.openqa.selenium.By;
-
-import cucumber.api.Scenario;
 import io.appium.java_client.ios.IOSElement;
-import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.interactions.Action;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -22,108 +18,100 @@ import java.net.URL;
 public class Calculator {
 
     public AppiumDriver<IOSElement> driver;
-	// object to hold string value of argument
-	protected String arg1_converted_to_String ="";
-	// create array to hold each digit of operand 
-	protected String[] First_Operand;
-	protected boolean mark_negative;
-    
-    
-     public void Unified_Method(double arg1, double arg2, String Operand) throws MalformedURLException {
+	protected String operand_in_String ="";		// Object to hold Operand in string format
+	protected String[] operand_in_Array;			/// Array to hold each digit of operand 
+	protected boolean is_Negative;
+    protected DesiredCapabilities capabilities;
 
-    	 	driver = new io.appium.java_client.ios.IOSDriver<IOSElement>(new URL("http://127.0.0.1:4622/wd/hub"), new DesiredCapabilities());
-    	 	// Click View -> Basic menu item so that calculator is in Basic mode before any operation is performed
-    	 	IOSElement Cal_View = (IOSElement)driver.findElement(By.xpath("/AXApplication[@AXTitle='Calculator']/AXMenuBar[0]/AXMenuBarItem[@AXTitle='View']/AXMenu[0]/AXMenuItem[@AXTitle='Basic']"));
-    	 	Cal_View.click();
-    	 	
-    	 	// clear the result screen before performing any operation
-        Click_Operand_Through_Description("clear");
-        // create an array of arguments passed to method
-        double[] input_arguments = {arg1, arg2};
-        // iterator through individual argument of method
-        for(int arguments_counter=0; arguments_counter<input_arguments.length; arguments_counter++)
-        {
-            // Key to mark operand as negative (if any)
-        		mark_negative = false;
+    public void init()
+    {
+    		/* key piece of code as we are connecting to local Appium Server running over 127.0.0.1 with port 4622 */
+    	    /* Specify desired capabilities required by Appium server*/
+    	
+        capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "mac");
+        capabilities.setCapability("deviceName", "mac");
+        capabilities.setCapability("app", "/Applications/Calculator.app");
+        try {
 
-            // determine if input operand contains fractional values or not
-            Determine_Fractional_Part_Existsance(input_arguments[arguments_counter]);	                   
-	        	// Method to iterate through String array holding operand and click buttons on Calculator
-            Iterate_Characters_and_Click();
-	        	// if its not the last operand, then click + button
-	        	if(arguments_counter<input_arguments.length-1)
-	        	{
-	        		// click respective Operator
-	        		Click_Operand_Through_Description(Get_Operand_With_Description(Operand));
-	        	}
-        } // for loop for arguments counter ends here
+        		/* Since we need to connect to Appium-for-Mac, so we need to create an object of AppiumDriver class*/
+			driver = new io.appium.java_client.ios.IOSDriver<IOSElement>(new URL("http://127.0.0.1:4622/wd/hub"), capabilities);	
+			// launch calculator application
+			driver.get("/Applications/Calculator.app");
+			
+			// Set calculator to Basic mode. click View -> Basic menu item 
+    	 		IOSElement Basic_View = (IOSElement)driver.findElement(By.xpath("/AXApplication[@AXTitle='Calculator']/AXMenuBar[0]/AXMenuBarItem[@AXTitle='View']/AXMenu[0]/AXMenuItem[@AXTitle='Basic']"));
+    	 		Basic_View.click();
 
-    	// click = operator to compute result. This is last step for certain computation operation
-    	Click_Operand_Through_Description(Get_Operand_With_Description("="));
-    }  // add method ends here
-    
-     
-     // ********************* =========================================**********************//	
- 	 // *********============ Determine if operand contains fractional value ========********//
-     // ********************* =========================================**********************//	
+		} catch (Exception e) {
+			System.out.println("\n ------- Unable to launch Calculator with Appium");
+        }
+    }
+	
+     public void Unified_Method(double arg1, double arg2, String Operand) throws MalformedURLException {    	 
         
-     // In following method, we will first determine if the input value contains fractional part or not
-     // The double value is then converted to string array, so that we can asses each individual character/digit
-     // get its related AXDescription code and use driver to click appropriate button on application.
-     public String Determine_Fractional_Part_Existsance(double input_operand)
-     {
-    		if(input_operand %1==0)
-        	{
-        		// convert double operand to integer and then cast to String object
-        		arg1_converted_to_String = Integer.toString((int)input_operand);            
-        		// create array to hold each digit of operand        
-        		First_Operand = new String[arg1_converted_to_String.length()];
-        		// convert string to array of String type
-        		First_Operand = arg1_converted_to_String.split("");	
-        	}
-        	else 
-        	{
-        		// convert double operand to string
-        		arg1_converted_to_String = Double.toString(input_operand);
-        		// create array to hold each digits of operand        
-        		First_Operand = new String[arg1_converted_to_String.length()];
-        		// convert string to string array
-        		First_Operand = arg1_converted_to_String.split("");
-        	}
-    		return arg1_converted_to_String;
-     }	// end of Determine_Fractional_Part_Existsance
+        // create an array of arguments passed to method
+        double[] input_args = {arg1, arg2};
+        // iterate through each value in Array
+        for(int counter=0; counter<input_args.length; counter++)
+        {
+            // Flag if input is negative
+        		is_Negative = false;
+
+	        		// determine of Operand is Integer or Double
+		        	if(input_args[counter]%1 ==0)
+		        		operand_in_String = Integer.toString((int)input_args[counter]); 	// No Fractional values
+		        	else
+		        		operand_in_String = Double.toString(input_args[counter]);			// Contains Fractional values
+		        		
+		        		// convert string to array, so that we can access individual digit/character
+		        		operand_in_Array = operand_in_String.split("");
+        			        		
+	        	// Method to iterate through String array holding operand and click buttons on Calculator
+            click_Operand_Buttons();
+	        
+            // Click Operator when all digits for operand are entered
+	        	if(counter<input_args.length-1)
+	        	{
+	        		// click respective Button.
+	        		click_Button(get_Button_Code(Operand));
+	        	}	        	
+        } // for loop for arguments counter ends here
+        click_Button(get_Button_Code("="));	// perform calculations
+    }  // Unified method ends here
+         
     
      
      // ********************* =========================================**********************//	
- 	 // ************============ Iterate in Array and call click method ===========**********//
+ 	 // ************============ Iterate Array and call click method ===========**********//
      // ********************* =========================================**********************//	
      
-     // Method to iterate through individual character/object inside String array and call click method
-     public void Iterate_Characters_and_Click()
+     public void click_Operand_Buttons()
      {
-    	 	// now we need to iterate through array which holds all digits/characters of particular operand
-     	for(int operand_counter = 0; operand_counter<arg1_converted_to_String.length(); operand_counter++)
+    	 	// Iterate through array which holds all digits/characters of operand
+     	for(int counter = 0; counter<operand_in_String.length(); counter++)
      	{
      		// if first character of operand is - sign then its negative value
-     		if(operand_counter == 0 & First_Operand[operand_counter].equals("-")) 
+     		if(counter == 0 & operand_in_Array[counter].equals("-")) 
      		{
      			// enable bit that negative value found
-     			mark_negative=true;
+     			is_Negative=true;
      		}
  			else 
  			{
  				// Get the AXDescription of iterating character and then click the button on calculator
  				try {
-						Click_Operand_Through_Description(Get_Operand_With_Description(First_Operand[operand_counter]));
-						// Once all digits are clicked, we need to toggle the value to negative (where applicable)
-						if(mark_negative==true & operand_counter==arg1_converted_to_String.length()-1)
+ 						click_Button(get_Button_Code(operand_in_Array[counter]));
+						// Once all digits are clicked, toggle value to negative (where applicable)
+						if(is_Negative==true & counter==operand_in_String.length()-1)
 			        		{
 		        				// when argument is negative, click +/- button
-		        				Click_Operand_Through_Description(Get_Operand_With_Description("+/-"));
+							click_Button(get_Button_Code("+/-"));
 			        		}
  					} catch (MalformedURLException e) {
 						// TODO Auto-generated catch block
-						System.out.println(" Error during object identification from Array and click on calculator Application "+e.getStackTrace());
+						System.out.println(" Error during object identification from Array "+e.getStackTrace());
  					}
  			}	        	
      	}
@@ -134,7 +122,7 @@ public class Calculator {
      // ********************* =========================================**********************//	
      
     // Get Unique Identifier of certain Operand
-    public String Get_Operand_With_Description(String Operand_Number)
+    public String get_Button_Code(String Operand_Number)
     {
     	String Return_code="";
     	switch(Operand_Number) 
@@ -172,26 +160,21 @@ public class Calculator {
 		    	case "/": Return_code = "divide";    	
 				break;
 		    	case "=": Return_code = "equals";    	
-				break;
-		    	case "C": Return_code = "clear";    	
-				break;
-		    	case "AC": Return_code = "clear";    	
-				break;
 	    	} // Case-Switch ends here
     return Return_code;
     }  // Get_Operand_Identifier ends here		
 	
 
     // ********************* =========================================**********************//	
-	// **********============ Click button based on AXDescription value ==========**********//
+	// **********=========== Click button based on AXDescription value ==========**********//
     // ********************* =========================================**********************//	
 
     // click object passed as argument to method
-	public IOSElement Click_Operand_Through_Description(String Identifier) throws MalformedURLException{
+	public IOSElement click_Button(String Identifier) throws MalformedURLException{
 			 	
 		IOSElement Found_Object = null;
 		try {
-			// Identify first Operand over calculator. Wait for 10 seconds until the control becomes visible
+			// Wait for 10 seconds until the control becomes visible
 			 Found_Object = (IOSElement)(new WebDriverWait(driver,10)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/AXApplication[@AXTitle='Calculator']/AXWindow[0]/AXGroup[1]/AXButton[@AXDescription='"+Identifier+"']")));
 			if (Found_Object != null) {
                Actions builder = new Actions(driver);
@@ -203,7 +186,7 @@ public class Calculator {
 			System.out.print("\n Object Not found....");
 		}
 		return Found_Object;
-	}  // Click_Operand_Through_Description method ends here. This method takes String based Description as an argument
+	}  
 	
     
 	// ********************* =========================================**********************//	
