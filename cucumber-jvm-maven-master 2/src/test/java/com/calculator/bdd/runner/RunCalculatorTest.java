e44aa567267c46e5a320d4a653a871a84e56078e
codewarior <nayyer.shahbaz@aspose.com>
2018-02-20 20:48:06 +0500

    Add files via upload
    RunCalculatorTest.java file is updated to have minimum content but maximum coverage/functionality.

--- cucumber-jvm-maven-master 2/src/test/java/com/calculator/bdd/runner/RunCalculatorTest.java
+++ cucumber-jvm-maven-master 2/src/test/java/com/calculator/bdd/runner/RunCalculatorTest.java
@@ -6,15 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        glue = "com.calculator.bdd.steps",
-        features = "classpath:cucumber/calculator.feature",
-        		plugin = { "html:target/cucumber-html-report",
-        		        "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
-        		        "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml" }
-)
+		glue = "com.calculator.bdd.steps", 
+		features = "classpath:cucumber/calculator.feature", 
+		plugin = {
+		"html:target/cucumber-html-report" })
 public class RunCalculatorTest {
 }
-
-
-//format={"pretty","html:target/Reports"}
-
